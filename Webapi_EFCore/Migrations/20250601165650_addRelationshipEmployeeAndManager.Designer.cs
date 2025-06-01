2ec500a48c0315883189c1b4ef1aa69c755a9cdd
VolumpUpz <132444587+VolumpUpz@users.noreply.github.com>
2025-06-02 00:00:36 +0700

    add-migration relationshipEmployeeAndManager

--- Webapi_EFCore/Migrations/20250601165650_addRelationshipEmployeeAndManager.Designer.cs
+++ Webapi_EFCore/Migrations/20250601165650_addRelationshipEmployeeAndManager.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webapi_EFCore.Data;
 
@@ -10,9 +11,11 @@ using Webapi_EFCore.Data;
 namespace Webapi_EFCore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250601165650_addRelationshipEmployeeAndManager")]
+    partial class addRelationshipEmployeeAndManager
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Webapi_EFCore/Migrations/20250601165650_addRelationshipEmployeeAndManager.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webapi_EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRelationshipEmployeeAndManager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmployeeProject_Employees_EmployeesEmployeeId",
+                table: "EmployeeProject");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmployeeProject_Projects_ProjectsProjectId",
+                table: "EmployeeProject");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EmployeeProject",
+                table: "EmployeeProject");
+
+            migrationBuilder.RenameTable(
+                name: "EmployeeProject",
+                newName: "EmployeeProjects");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EmployeeProject_ProjectsProjectId",
+                table: "EmployeeProjects",
+                newName: "IX_EmployeeProjects_ProjectsProjectId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Managers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Managers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EmployeeProjects",
+                table: "EmployeeProjects",
+                columns: new[] { "EmployeesEmployeeId", "ProjectsProjectId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmployeeProjects_Employees_EmployeesEmployeeId",
+                table: "EmployeeProjects",
+                column: "EmployeesEmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmployeeProjects_Projects_ProjectsProjectId",
+                table: "EmployeeProjects",
+                column: "ProjectsProjectId",
+                principalTable: "Projects",
+                principalColumn: "ProjectId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmployeeProjects_Employees_EmployeesEmployeeId",
+                table: "EmployeeProjects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmployeeProjects_Projects_ProjectsProjectId",
+                table: "EmployeeProjects");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EmployeeProjects",
+                table: "EmployeeProjects");
+
+            migrationBuilder.RenameTable(
+                name: "EmployeeProjects",
+                newName: "EmployeeProject");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EmployeeProjects_ProjectsProjectId",
+                table: "EmployeeProject",
+                newName: "IX_EmployeeProject_ProjectsProjectId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Managers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Managers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EmployeeProject",
+                table: "EmployeeProject",
+                columns: new[] { "EmployeesEmployeeId", "ProjectsProjectId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmployeeProject_Employees_EmployeesEmployeeId",
+                table: "EmployeeProject",
+                column: "EmployeesEmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmployeeProject_Projects_ProjectsProjectId",
+                table: "EmployeeProject",
+                column: "ProjectsProjectId",
+                principalTable: "Projects",
+                principalColumn: "ProjectId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
