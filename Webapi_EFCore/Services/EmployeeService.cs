997189e4a54dea70324e1a22027adf5ea5b9acaa
VolumpUpz <strongroom.volumpupz@gmail.com>
2025-06-03 16:27:17 +0700

    add automapper to employee

---
+++ Webapi_EFCore/Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Webapi_EFCore.DTOs;
+using Webapi_EFCore.Models;
+using Webapi_EFCore.Repositories.Interfaces;
+using Webapi_EFCore.Services.Interfaces;
+
+namespace Webapi_EFCore.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+        public EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EmployeeDTO>> GetAllAsync()
+        {
+            var employees = await _employeeRepository.GetAllAsync();
+            return _mapper.Map<List<EmployeeDTO>>(employees);
+        }
+    }
+}
